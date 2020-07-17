3d54f60c3c903caa3901dd5f50b094742e275fd5
clecio-sousa <clecio1201@hotmail.com>
2020-07-16 23:38:33 -0300

    alteracao model

--- app/cliente/models.py
+++ app/cliente/models.py
@@ -7,7 +7,7 @@ from django.db import models
 # Create your models here.
 class Endereco(models.Model):
     endereco = models.CharField(max_length=100)
-    bairro = models.DateTimeField(max_length=100)
+    bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     uf = models.CharField(max_length=100)
     latitude = models.FloatField()
